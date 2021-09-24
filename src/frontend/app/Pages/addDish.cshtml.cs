48748e2ddfe78b89b95348b019dd20b384f14f10
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2021-09-24 14:36:28 +0200

    updated adddish and styling

--- src/frontend/app/Pages/addDish.cshtml.cs
+++ src/frontend/app/Pages/addDish.cshtml.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestSharp;
 
 namespace app.Pages
 {
     public class addDishModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+        [BindProperty]
+        public string Name { get; set; }
 
         public void OnPost()
         {
-            
+            var recipe = new Recipe {Name = Name};
+            var client = new RestClient("https://restapi-cosmosdb.azurewebsites.net/api");
+            var request = new RestRequest("/recipe?");
+            request.AddJsonBody(recipe);
+            var response = client.Post<Recipe>(request);
         }
     }
 }
